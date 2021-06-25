2c061e6a3d5fa5cd96a6d7826a96d7bf3f9f490b
yuwangi <yuwangifeng@163.com>
2021-06-25 11:20:59 +0800

    Update sports.sh

--- sports.sh
+++ sports.sh
@@ -14,4 +14,8 @@ step=$(rand 17500 23000)
 user=secrets.PHONE
 password=secrets.PASSWORD
 
+echo user
+echo password
+echo step
+
 curl -X GET --header 'Accept: application/json' 'https://service-cunaufvn-1302033217.bj.apigw.tencentcs.com/release/xm?user=${user}&password=${password}&step=${step}'
